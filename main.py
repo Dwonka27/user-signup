d44c3a7eca30b25597e8c7b58544ca6e386c1472
Derek Czerwonka <Dwonka27@Gmail.com>
2017-10-05 00:47:31 -0500

    making good progress

--- main.py
+++ main.py
@@ -10,8 +10,19 @@ jinja_env = jinja2.Environment(
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+@app.route("/home")
+def display_home():
+    return render_template("home.html", title="Sign Me Up")
 
-@app.route("/home", methods=["POST", "GET"])
+def valid_username(name):
+    
+    if len(name) == 0 or < 3:
+        return error
+    elif name 
+
+
+
+@app.route("/home", methods=["POST"])
 def home():
     
     username = request.form['username']
@@ -20,16 +31,20 @@ def home():
     email = request.form['email']
 
     user_error = ""
-    
     pass_error = ""
     email_error = ""
 
-    
+    if username
 
-    return render_template("home.html", title="Sign Me Up")
+    return render_template("home.html", title="Sign me up")
 
 
 @app.route("/welcome")
 def welcome():
 
+    user_name = request.form['username']
     return render_template("welcome.html", title="HEY THERE!!")
+
+
+
+app.run()
